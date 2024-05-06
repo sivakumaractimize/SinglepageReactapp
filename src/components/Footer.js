d9523a7099b03fc1d856351a34a4f590318d295f
sivakumaractimize <163836516+sivakumaractimize@users.noreply.github.com>
2024-05-06 18:27:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,68 +1,63 @@
-import React, { useState } from "react";
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Instagram ,Facebook,Youtube,Linkedin} from 'lucide-react';
-const Footer =()=>{
 
-    return(
 
+const Footer = () => {
+    return (
         <>
-        <div className=""
-        style={{
-            backgroundColor:'black',
-         }}>
-        
-        <div className="container text-center">
-          <div className="row">
-             <div class="col mt-5">
-             <img src='./assets/images/cuplogo.png' alt="Your Logo"  />
-              </div>
-             <div className="col fs-6 mt-5 text-center">
-                <ul className=" list-unstyled text-white d-flex row ">
-                <li className="fw-bolder">HOME</li>
-                <li className="fw-bolder mt-2 ">SHOP</li>
-                <li className="fw-bolder mt-2">ABOUT</li>
-                <li className="fw-bolder mt-2 ">ORDERS</li>
+            <div className="bg-dark">
+                <div className="container text-center py-5">
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col-md-3 mb-4 mb-md-0">
+                            <img src='./assets/images/cuplogo.png' alt="Your Logo" className="img-fluid" />
+                        </div>
+                        <div className="col-md-6 mb-4 mb-md-0">
+                            <ul className="list-unstyled text-white d-flex flex-column flex-md-row">
+                                <li className="fw-bolder mb-2 mb-md-0 me-md-4">HOME</li>
+                                <li className="fw-bolder mb-2 mb-md-0 me-md-4">SHOP</li>
+                                <li className="fw-bolder mb-2 mb-md-0 me-md-4">ABOUT</li>
+                                <li className="fw-bolder">ORDERS</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3 text-white">
+                            <div className="row gy-1 gy-md-0">
+                            <div className="col-3 col-md-3 mb-3 mb-md-0">
+                           <img src="./assets/images/instagram (1).svg" className="img-fluid icon" style={{ filter: "invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)" }} alt="Instagram" />
+                          </div>
 
-                </ul>
-              </div>
-              <div className="col navicon mt-5">
-              <Instagram size={40} strokeWidth={1.5} />
-              <Facebook size={40} strokeWidth={1.5} />
-              <Youtube size={40} strokeWidth={1.5} />
-              <Linkedin className="ms-2 mb-1" size={40} strokeWidth={1.5} />
-              </div>
-              
+                                <div className="col-3 col-md-3 mb-3 mb-md-0">
+                                <img src="./assets/images/facebook (3).svg" className="img-fluid" style={{ filter: "invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)" }}></img>
+                                   
+                                </div>
+                                <div className="col-3 col-md-3 mb-3 mb-md-0">
+                                <img src="./assets/images/youtube.svg" className="img-fluid" style={{ filter: "invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)" }}></img>
+                                   
+                                </div>
+                                <div className="col-3 col-md-3 mb-3 mb-md-0">
+                                   <img src="./assets/images/linkedin.svg" className="img-fluid" style={{ filter: "invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)" }}></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-        </div>
-             
-                   
-
-        <div className="container text-center text-white mt-5 ">
-              <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 ">
-              <div className="col mb-5">© 2024 Dann Good Coffee</div>
-              <div className="col mb-5">
-              <a href="" className="text-reset text-decoration-white">Privacy Policy</a>
-               </div>
-              <div className="col mb-5">
-              <a href="" className="text-reset text-decoration-white">Terms of Service</a>
-              </div>
-              <div className="col mb-5">
-               <a href="" className="text-reset text-decoration-white">Coffee Settings</a>
-              </div>
-               </div>
-    </div>                  
-
-                   
-
-
-
-      </div>
-
-    </div>
-        
+                <div className="container-fluid text-center text-white py-4">
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-md-3 mb-3 mb-md-0">© 2024 Dann Good Coffee</div>
+                        <div className="col-12 col-md-3 mb-3 mb-md-0">
+                            <a href="#" className="text-reset text-decoration-white">Privacy Policy</a>
+                        </div>
+                        <div className="col-12 col-md-3 mb-3 mb-md-0">
+                            <a href="#" className="text-reset text-decoration-white">Terms of Service</a>
+                        </div>
+                        <div className="col-12 col-md-3">
+                            <a href="#" className="text-reset text-decoration-white">Coffee Settings</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
+
 export default Footer;
-
-
